e6656a885f4a1f9b6ad2ed366e9b2c5bf147f2ea
simon-2006 <341037@student.mboutrecht.nl>
2026-07-03 10:40:18 +0200

    Registreer behandeling product routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\BehandelingController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -23,6 +25,10 @@ Route::match(['get', 'post'], '/uitloggen', [AuthenticatedSessionController::cla
 Route::middleware('auth')->group(function (): void {
     Route::view('/klanten', 'klanten.index')->name('klanten.index')->can('view-owner-pages');
     Route::view('/medewerkers', 'medewerkers.index')->name('medewerkers.index')->can('view-owner-pages');
-    Route::view('/behandelingen', 'behandelingen.index')->name('behandelingen.index')->can('view-owner-pages');
+    Route::get('/behandelingen', [BehandelingController::class, 'index'])->name('behandelingen.index')->can('view-owner-pages');
+    Route::get('/behandelingen/{behandeling}/producten', [ProductController::class, 'perBehandeling'])->name('behandelingen.producten.index')->can('view-owner-pages');
+    Route::get('/behandelingen/{behandeling}/producten/{product}', [ProductController::class, 'showPerBehandeling'])->name('behandelingen.producten.show')->can('view-owner-pages');
+    Route::get('/behandelingen/{behandeling}/producten/{product}/wijzigen', [ProductController::class, 'editPerBehandeling'])->name('behandelingen.producten.edit')->can('view-owner-pages');
+    Route::put('/behandelingen/{behandeling}/producten/{product}', [ProductController::class, 'updatePerBehandeling'])->name('behandelingen.producten.update')->can('view-owner-pages');
     Route::view('/producten', 'producten.index')->name('producten.index')->can('view-owner-pages');
 });
