583d453115879e34c19a9ae338c0e97c57a362a4
OdiMatar <340712@student.mboutrecht.nl>
2026-07-03 10:44:32 +0200

    Maak klantmodellen geschikt voor overzicht

--- app/Models/Klant.php
+++ app/Models/Klant.php
@@ -2,8 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Klant extends Model
 {
+    protected $table = 'Klant';
+
+    protected $primaryKey = 'Id';
+
+    public $timestamps = false;
+
+    /**
+     * Haalt actieve klanten met hun gekoppelde contactgegevens op via de databaseprocedure.
+     */
+    public static function zoekMetContactgegevens(?string $postcode = null): Collection
+    {
+        $klanten = DB::select('CALL sp_get_klanten_met_contactgegevens(?)', [$postcode]);
+
+        return new Collection($klanten);
+    }
 }
--- app/Models/TechnicalLog.php
+++ app/Models/TechnicalLog.php
@@ -6,4 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class TechnicalLog extends Model
 {
+    protected $table = 'TechnicalLog';
+
+    protected $primaryKey = 'Id';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'Onderdeel',
+        'Melding',
+        'Context',
+    ];
 }
