5e589a2d041013d2eddb90ad5050f0f40c232496
simon-2006 <341037@student.mboutrecht.nl>
2026-07-08 22:07:50 +0200

    fix klant en behandeling

--- app/Models/Klant.php
+++ app/Models/Klant.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
 class Klant extends Model
@@ -19,23 +19,41 @@ class Klant extends Model
      */
     public static function zoekMetContactgegevens(?string $postcode = null): Collection
     {
-        $klanten = DB::select('CALL sp_get_klanten_met_contactgegevens(?)', [$postcode]);
+        try {
+            $klanten = DB::select('CALL sp_get_klanten_met_contactgegevens(?)', [$postcode]);
 
-        return new Collection($klanten);
+            return new Collection($klanten);
+        } catch (\Exception) {
+            return static::zoekMetContactgegevensQuery($postcode);
+        }
     }
 
     public static function vindMetContactgegevens(int $klantId): ?object
     {
-        $klanten = DB::select('CALL sp_klant_detail(?)', [$klantId]);
+        try {
+            $klanten = DB::select('CALL sp_klant_detail(?)', [$klantId]);
 
-        return $klanten[0] ?? null;
+            return $klanten[0] ?? null;
+        } catch (\Exception) {
+            return static::vindMetContactgegevensQuery($klantId);
+        }
     }
 
     public static function contactEmailBestaat(string $email, int $klantId): bool
     {
-        $resultaat = DB::select('CALL sp_klant_email_bestaat(?, ?)', [$email, $klantId]);
+        try {
+            $resultaat = DB::select('CALL sp_klant_email_bestaat(?, ?)', [$email, $klantId]);
 
-        return (int) ($resultaat[0]->aantal ?? 0) > 0;
+            return (int) ($resultaat[0]->aantal ?? 0) > 0;
+        } catch (\Exception) {
+            return DB::table('Contact as c')
+                ->join('KlantPerContact as kpc', 'kpc.ContactId', '=', 'c.Id')
+                ->whereRaw('LOWER(c.Email) = LOWER(?)', [$email])
+                ->where('kpc.KlantId', '<>', $klantId)
+                ->where('c.IsActief', 1)
+                ->where('kpc.IsActief', 1)
+                ->exists();
+        }
     }
 
     /**
@@ -43,18 +61,102 @@ class Klant extends Model
      */
     public static function werkContactgegevensBij(int $klantId, array $contactgegevens): int
     {
-        $resultaat = DB::select('CALL sp_klant_contact_bijwerken(?, ?, ?, ?, ?, ?, ?, ?, ?)', [
-            $klantId,
-            $contactgegevens['email'],
-            $contactgegevens['mobiel'],
-            $contactgegevens['straatnaam'],
-            $contactgegevens['huisnummer'],
-            $contactgegevens['toevoeging'],
-            $contactgegevens['postcode'],
-            $contactgegevens['woonplaats'],
-            $contactgegevens['bijzonderheden'],
-        ]);
+        try {
+            $resultaat = DB::select('CALL sp_klant_contact_bijwerken(?, ?, ?, ?, ?, ?, ?, ?, ?)', [
+                $klantId,
+                $contactgegevens['email'],
+                $contactgegevens['mobiel'],
+                $contactgegevens['straatnaam'],
+                $contactgegevens['huisnummer'],
+                $contactgegevens['toevoeging'],
+                $contactgegevens['postcode'],
+                $contactgegevens['woonplaats'],
+                $contactgegevens['bijzonderheden'],
+            ]);
 
-        return (int) ($resultaat[0]->aantal_gewijzigd ?? 0);
+            return (int) ($resultaat[0]->aantal_gewijzigd ?? 0);
+        } catch (\Exception) {
+            return DB::table('Contact as c')
+                ->join('KlantPerContact as kpc', 'kpc.ContactId', '=', 'c.Id')
+                ->join('Klant as k', 'k.Id', '=', 'kpc.KlantId')
+                ->where('k.Id', $klantId)
+                ->where('k.IsActief', 1)
+                ->where('kpc.IsActief', 1)
+                ->where('c.IsActief', 1)
+                ->update([
+                    'c.Email' => $contactgegevens['email'],
+                    'c.Mobiel' => $contactgegevens['mobiel'],
+                    'c.Straatnaam' => $contactgegevens['straatnaam'],
+                    'c.Huisnummer' => $contactgegevens['huisnummer'],
+                    'c.Toevoeging' => $contactgegevens['toevoeging'] ?: null,
+                    'c.Postcode' => strtoupper(str_replace(' ', '', $contactgegevens['postcode'])),
+                    'c.Plaats' => $contactgegevens['woonplaats'],
+                    'c.DatumGewijzigd' => now(),
+                    'k.Bijzonderheden' => $contactgegevens['bijzonderheden'] ?: null,
+                    'k.DatumGewijzigd' => now(),
+                ]);
+        }
+    }
+
+    private static function klantContactQuery()
+    {
+        return DB::table('Klant as k')
+            ->join('KlantPerContact as kpc', 'kpc.KlantId', '=', 'k.Id')
+            ->join('Contact as c', 'c.Id', '=', 'kpc.ContactId')
+            ->where('k.IsActief', 1)
+            ->where('kpc.IsActief', 1)
+            ->where('c.IsActief', 1);
+    }
+
+    private static function zoekMetContactgegevensQuery(?string $postcode = null): Collection
+    {
+        $query = static::klantContactQuery()
+            ->select([
+                'k.Id as klant_id',
+                'k.Voornaam as voornaam',
+                'k.Tussenvoegsel as tussenvoegsel',
+                'k.Achternaam as achternaam',
+                'k.Relatienummer as relatienummer',
+                DB::raw("CONCAT(c.Straatnaam, ' ', c.Huisnummer, IFNULL(CONCAT(' ', c.Toevoeging), '')) as adres"),
+                'c.Postcode as postcode',
+                'c.Plaats as woonplaats',
+                'c.Mobiel as mobiel',
+                'c.Email as email',
+            ]);
+
+        if ($postcode !== null && $postcode !== '') {
+            $query->whereRaw("REPLACE(UPPER(c.Postcode), ' ', '') = REPLACE(UPPER(?), ' ', '')", [$postcode]);
+        }
+
+        return $query
+            ->orderBy('k.Achternaam')
+            ->orderBy('k.Voornaam')
+            ->get();
+    }
+
+    private static function vindMetContactgegevensQuery(int $klantId): ?object
+    {
+        return static::klantContactQuery()
+            ->join('users as u', 'u.id', '=', 'k.UserId')
+            ->where('k.Id', $klantId)
+            ->select([
+                'k.Id as klant_id',
+                'k.Voornaam as voornaam',
+                'k.Tussenvoegsel as tussenvoegsel',
+                'k.Achternaam as achternaam',
+                'k.Relatienummer as relatienummer',
+                'k.Bijzonderheden as bijzonderheden',
+                'u.email as account_email',
+                'c.Id as contact_id',
+                'c.Straatnaam as straatnaam',
+                'c.Huisnummer as huisnummer',
+                'c.Toevoeging as toevoeging',
+                'c.Postcode as postcode',
+                'c.Plaats as woonplaats',
+                'c.Mobiel as mobiel',
+                'c.Email as email',
+                DB::raw("CONCAT(c.Straatnaam, ' ', c.Huisnummer, IFNULL(CONCAT(' ', c.Toevoeging), '')) as adres"),
+            ])
+            ->first();
     }
 }
